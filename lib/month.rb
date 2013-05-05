70423141f559de5b91baccd21412f72363b9eec8
sjalal <sjalal0228@gmail.com>
2013-05-05 18:13:02 -0500

    Update month.rb

--- lib/month.rb
+++ lib/month.rb
@@ -8,9 +8,14 @@ class Month
     end 
 
     def month_name
-        month_names = ['January','February','March','April','May','June','July','August','September','October','November','December']
-        month_names[@month - 1]
-    end
+        monthNames = {1 => 'January', 2 => 'February', 3 => 'March', 4 => 'April', 5 => 'May', 6 => 'June', 7 => 'July', 8 => 'August', 9 => 'September', 10 => 'October', 11 => 'November', 12 =>'December'}
+        monthNames[@month]
+    end 
+
+    # def month_name
+    #     month_names = ['January','February','March','April','May','June','July','August','September','October','November','December']
+    #     month_names[month - 1]
+    # end
 
     def num_of_days
          days_in_month=[31,28,31,30,31,30,31,31,30,31,30,31]
@@ -21,20 +26,20 @@ class Month
       end
     end
     
-    def start_day
-             q = 1
-             month = @month
-             yr = @year
-          if month == 1 || month == 2 # if month equal 1 or 2 make mothe 13 or 14 it represents January and February in Zeller's algorithm
+    def zeller_algorithm 
+           q = 1
+           month = @month
+           yr = @year
+          if month == 1 || month == 2 # if month equal 1 or 2 make them 13 or 14 it represents January and February in Zeller's algorithm
              month += 12
              yr = @year - 1
          end 
             #Zeller's congruence http://en.wikipedia.org/wiki/Zeller's_congruence
-             start_num = (q + ((month + 1)*26  / 10) + yr + (yr / 4) + (6*(yr / 100)) + (yr / 400)) % 7
-          if start_num == 0
-             start_num = 7
-         end 
-             start_num
+            start_num = (q + ((month + 1)*26  / 10) + yr + (yr / 4) + (6*(yr / 100)) + (yr / 400)) % 7
+            if start_num == 0
+              start_num = 7
+            end
+            start_num
     end 
 
     def leap_year?
@@ -50,17 +55,29 @@ class Month
     end 
 
     def month_year_header
-       " #{month_name} #{@year}".center(20).rstrip
+     "#{month_name} #{@year}"
     end 
 
     def day_header
-       "Su Mo Tu We Th Fr Sa" 
+     "Su Mo Tu We Th Fr Sa" 
     end 
 
-    # def format_cal
-    # end
-
-    # def print_cal
-    # end 
+    def format_month
+        start_day = zeller_algorithm-1
+        layout = " "
+        start_day.times do layout.prepend("   ") end 
+        layout += (1..9).to_a.join("  ")
+        layout += " "
+        layout += (10..num_of_days).to_a.join(" ")
+        new_layout = layout.scan(/.{1,21}/)
+        new_layout.each  {|x| x.rstrip!}
+        new_layout
+    end
+    
+    def print_month 
+        first_line = month_year_header.center(20)
+        second_line = day_header
+        format_month.unshift(second_line).unshift(first_line)
+    end
 
 end 
