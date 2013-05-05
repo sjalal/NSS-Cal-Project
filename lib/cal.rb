5083012768d52f7e30893249d699199c1afaab11
sjalal <sjalal0228@gmail.com>
2013-05-05 18:11:30 -0500

    Update cal.rb

--- lib/cal.rb
+++ lib/cal.rb
@@ -1,12 +1,18 @@
-require 'year'
-require 'month'
+require_relative 'month'
+require_relative 'year'
 
+  # def cal_header
+  #   month = ARGV[0]
+  #   year = ARGV[1]
+  #   puts `cal #{month} #{year}`
+  # end 
 
-  def cal_header
-    month = ARGV[0]
-    year = ARGV[1]
-    puts `cal #{month} #{year}`
-  end 
+if  ARGV.length == 1
+  month = ARGV[0].to_i
+  year = ARGV[1].to_i
+  puts Month.new(month, year).print_month
+end
+
 
 
 
